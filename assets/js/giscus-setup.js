45144e39c852ab55220156054bf47d083f341edb
ZoExOr <104420617+ZoExOr@users.noreply.github.com>
2026-01-15 14:49:16 +0000

    Deploying to gh-pages from @ ZoExOr/zoexor.github.io@140e9add8db1dd9606e01629ce768bc5c1a36d96 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ZoExOr/ZoExOr.github.io",
+    "data-repo": "ZoExOr/zoexor.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
